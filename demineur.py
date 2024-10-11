0705db16d26e2c4141b34e8021034b6cb56bc0a5
Your Name <you@example.com>
2024-10-11 11:48:05 +0200

    ajout quadrillage

--- demineur.py
+++ demineur.py
@@ -1,7 +1,5 @@
 import pygame #IMPORTATION PYGAME
 
-pygame.init() #INITIALISATION PYGAME
-
  # INITIALISATION HAUTEUR / LARGEUR FENÊTRE
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
@@ -10,15 +8,39 @@ SCREEN_HEIGHT = 400
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
 
-# AFFICHAGE FENÊTRE
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+def main():
+    global SCREEN
+    pygame.init() #INITIALISATION PYGAME
+    
+    # AFFICHAGE FENÊTRE
+    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # DEFINITION FENÊTRE
+    
+    run = True
+    while run:
+        SCREEN.fill(BLACK)  # BACKGROUND COLOR NOIRE
+        
+        drawGrid() # APPEL FONCTION POUR DESSINER LA GRILLE 
+        
+        # AJOUT D'ITERATIONS DANS LES EVENEMENTS DEFINIS  PAR PYGAME
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # POUR FERMER LA FENÊTRE
+                run = False
+                
+        pygame.display.update() # MAJ DE L4AFFICHAGE
+    
+    pygame.quit()
+                #sys.exit()
+                
+    
 
-run = True
-while run:
-
-# AJOUT D'ITERATIONS DANS LES EVENEMENTS DEFINIS  PAR PYGAME
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: # POUR FERMER LA FENÊTRE
-            run = False
-
-pygame.quit()
+def drawGrid():
+    blockSize = 20 # TAILLE DES CASES
+    for x in range (0, SCREEN_WIDTH, blockSize) :
+        for y in range (0, SCREEN_HEIGHT, blockSize) :
+            # Rect(left, top, width, height)
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            # rect(surface, color, rectangle à dessiner, largeur)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+            
+if __name__ == "__main__":
+    main()
