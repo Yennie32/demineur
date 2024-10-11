a7835ad42e0ebbc2fe5a4798ca4fcab12eb6be54
Your Name <you@example.com>
2024-10-11 16:26:02 +0200

    frames OK, creating cells

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
+from cell import Cell # import the Cell class from the file cell.py
 import settings # import du fichier settings
 import utils
+import random 
+
 
 root  = Tk() # Window instanciation. Tk stands for toolkit and was developed as the GUI library for the scripting language tcl (tool command language).
 # Overide the setting of window
@@ -12,8 +15,8 @@ root.resizable(False, False) # window can't change size
 # Defining frames in the window
 top_frame = Frame(
     root, # where it should display
-    bg = "red", # background color # TODO change later
-    width=utils.width_prct(25),
+    bg = "#640D5F", # background color # TODO change later
+    width=settings.WIDTH,
     height =utils.height_prct(25)
     ) 
 
@@ -25,16 +28,47 @@ top_frame.place(
 
 left_frame = Frame(
     root, # where it should display
-    bg = "blue", # background color # TODO change later
-    width= 360,
-    height = 540
+    bg = "#640D5F", # background color # TODO change later
+    width= utils.width_prct(25),
+    height =utils.height_prct(75)
     ) 
 
 left_frame.place(
     x = 0, 
-    y = 180
+    y =utils.height_prct(25)
 )
 
+center_frame = Frame(
+    root, # where it should display
+    bg = "#640D5F", # background color # TODO change later
+    width= utils.width_prct(75),
+    height =utils.height_prct(75)
+)
+
+center_frame.place(
+    x = utils.width_prct(25), 
+    y =utils.height_prct(25)
+)
+
+c1=Cell()
+c1.create_btn_object(center_frame)
+c1.cell_btn_object.place(
+    x=0,
+    y=0
+)
+
+
+
+
+
+
+# BUTTON CREATION TO DELETE
+# btn1 = Button (
+#     center_frame,
+#     bg="blue", #position
+#     text="first button"
+# )
+# btn1.place(x=0, y=0)
 
 # Run the window
 root.mainloop() # displays the window and waits for the user's action
